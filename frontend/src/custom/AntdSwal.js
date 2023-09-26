74b75eea68eb8e779b3f1a85f8c8d54d3831127a
Devjung <dungthemai20@gmail.com>
2023-09-26 10:27:19 +0700

    update

--- frontend/src/custom/AntdSwal.js
+++ frontend/src/custom/AntdSwal.js
@@ -3,8 +3,8 @@ import Swal from "sweetalert2";
 const AntdSwal = Swal.mixin({
   width: "450px",
   customClass: {
-    confirmButton: "ant-btn css-dev-only-do-not-override-190m0jy ant-btn-primary ant-btn-lg",
-    cancelButton: "ant-btn css-dev-only-do-not-override-190m0jy ant-btn-primary ant-btn-lg ant-btn-dangerous mr-3",
+    confirmButton: "ant-btn css-dev-only-190m0jy ant-btn-primary ant-btn-lg",
+    cancelButton: "ant-btn css-dev-only-190m0jy ant-btn-primary ant-btn-lg ant-btn-dangerous mr-3",
   },
   buttonsStyling: false,
 });
